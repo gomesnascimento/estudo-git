4bad89f55d69674097e267f168326a7d9a63cd72
Carlos Eduardo <gomesnascimento3066@gmail.com>
2023-12-14 16:41:01 -0300

    Aprendendo funções

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-function ehMinhaIdade(idade){
+// function ehMinhaIdade(idade){
     // if(idade >= 18){
     //     return true;
     // } else {
     //     return false ;
     // }
-    return idade >= 18;
-}
+//     return idade >= 18;
+// }
 
-console.log (ehMinhaIdade(19));
+// console.log (ehMinhaIdade(19));
+
+function soma(){
+    console.log(63 + 32);
+}
+soma();
+
+function diminuir(){
+    console.log(2023 - 1990);
+}
+diminuir();
+
+function somar(idade1,idade2){
+    const resul = idade1 + idade2
+    console.log(resul)
+
+}
+somar(63,32);
+
+function diminuri(idade1,idade2){
+    const resul2 = idade1 - idade2
+    console.log(resul2);
+}
+diminuir(2023,1990);
+
+
+
+
